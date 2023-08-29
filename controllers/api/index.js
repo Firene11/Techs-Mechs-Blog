39a1776c4566ef7f2f8a31362294472a26455639
Fih <124883284+Firene11@users.noreply.github.com>
2023-08-29 17:17:53 -0300

    added configuration and controllers, setting up blog db

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,1 +1,8 @@
 //This file uses a router "require express and user routes"
+
+const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+
+router.use('/users', userRoutes);
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,1 +1,11 @@
 // routes api and home routes and expoorts them
+
+const router = require('express').Router();
+
+const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoutes');
+
+router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
+
+module.exports = router;
